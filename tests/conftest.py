612aad53d37102decb9efdcbcf284d86f5c4077f
daichi_yamaoka <daichi_yamaoka@bita.jp>
2020-08-30 18:34:37 +0900

    chapter4

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,8 @@
 import pytest
 
-@pytest.fixture()
-def tasks_db():
-    print("Start db connection.")
+@pytest.fixture(scope='session')
+def tasks_db(tmpdir_factory):
+    temp_dir = tmpdir_factory.mktemp('temp')
+    print("Start db connection.", temp_dir)
     yield
     print("Teardown db connection.")
